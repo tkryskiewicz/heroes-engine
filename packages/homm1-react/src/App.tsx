528e0e5cc74c74905cb34ff4745aa7023beab298
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2018-09-26 19:23:31 +0200

    setup Homm1 React UI package

---
+++ packages/homm1-react/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export class App extends React.Component {
+  public render() {
+    return (
+      <>
+        <h1>
+          heroes-engine
+        </h1>
+        <p>
+          A Heroes of Might and Magic engine written in TypeScript.
+        </p>
+      </>
+    );
+  }
+}
