bd03bdc206d63a03d45db0ada597718ba866c919
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2018-10-02 13:41:07 +0200

    clear selected locator after closing hero window

--- packages/homm1-state/src/heroWindow/index.ts
+++ packages/homm1-state/src/heroWindow/index.ts
@@ -1,3 +1,3 @@
-export { openHeroWindow, closeHeroWindow } from "./actions";
+export { HeroWindowAction, HeroWindowActionType, openHeroWindow, closeHeroWindow } from "./actions";
 export { heroWindowReducer } from "./reducers";
 export { HeroWindowState } from "./state";
--- packages/homm1-state/src/locators/reducers.ts
+++ packages/homm1-state/src/locators/reducers.ts
@@ -1,14 +1,24 @@
+import { HeroWindowAction } from "../heroWindow";
+import { HeroWindowActionType } from "../heroWindow/actions";
 import { LocatorsAction, LocatorsActionType } from "./actions";
 import { LocatorsState } from "./state";
 
 const initialState: LocatorsState = {};
 
-export const locatorsReducer = (state: LocatorsState = initialState, action: LocatorsAction): LocatorsState => {
+export const locatorsReducer = (
+  state: LocatorsState = initialState,
+  action: LocatorsAction | HeroWindowAction,
+): LocatorsState => {
   switch (action.type) {
     case LocatorsActionType.Select:
       return {
         ...state,
         selectedLocator: action.locator,
+      };
+    case HeroWindowActionType.Close:
+      return {
+        ...state,
+        selectedLocator: undefined,
       };
     default:
       return state;
