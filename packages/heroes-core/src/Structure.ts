6fe1ad56c0629235aacd33e61251115dbd3c6c97
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2018-11-05 22:14:40 +0100

    implement building dwellings

--- packages/heroes-core/src/Structure.ts
+++ packages/heroes-core/src/Structure.ts
@@ -7,6 +7,11 @@ export interface Dwelling {
   cost: Resources;
 }
 
+const buildDwelling = (dwelling: Dwelling): Dwelling => ({
+  ...dwelling,
+  availableCount: dwelling.growth,
+});
+
 export interface Structure {
   id: string;
   cost: Resources;
@@ -16,5 +21,8 @@ export interface Structure {
 
 export const buildStructure = (structure: Structure): Structure => ({
   ...structure,
+  dwelling: structure.dwelling ?
+    buildDwelling(structure.dwelling) :
+    undefined,
   isBuilt: true,
 });
