d0052133150f4a9188b741fcaef5ddf1861a1b84
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2019-01-02 17:03:37 +0100

    add README for spell book window

---
+++ packages/heroes-homm1-react/src/SpellBookWindow/README.md
@@ -0,0 +1,21 @@
+# Spell Book Window
+
+- displays combat spells by default
+
+## Combat Spells
+
+![Combat Spells](./assets/combat-spells.png "Combat Spells")
+
+Displays only combat spells.
+
+## Adventure Spells
+
+![Adventure Spells](./assets/adventure-spells.png "Adventure Spells")
+
+Displays only adventure spells.
+
+## Exit
+
+![Exit](./assets/exit.png "Exit")
+
+Closes window.
--- packages/heroes-homm1-react/src/SpellBookWindow/SpellBookWindow.story.tsx
+++ packages/heroes-homm1-react/src/SpellBookWindow/SpellBookWindow.story.tsx
@@ -2,10 +2,14 @@ import { action } from "@storybook/addon-actions";
 import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { withReadme } from "storybook-readme";
+
+import Readme = require("./README.md");
 
 import { SpellBookWindow } from "./SpellBookWindow";
 
 storiesOf("SpellBookWindow", module)
+  .addDecorator(withReadme(Readme))
   .add("default", () => (
     <SpellBookWindow
       visible={boolean("Visible", true)}
