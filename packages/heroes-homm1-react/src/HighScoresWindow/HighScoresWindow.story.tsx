5b736c3995130130ee768efbe6250e3b65b080b9
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2019-01-05 22:03:35 +0100

    improve high scores window readme

--- packages/heroes-homm1-react/src/HighScoresWindow/HighScoresWindow.story.tsx
+++ packages/heroes-homm1-react/src/HighScoresWindow/HighScoresWindow.story.tsx
@@ -2,8 +2,11 @@ import { action } from "@storybook/addon-actions";
 import { select } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { withReadme } from "storybook-readme";
 
 import { GameType, initialCampaignGameScores, initialStandardGameScores } from "heroes-homm1";
+
+import Readme = require("./README.md");
 
 import { HighScoresWindow } from "./HighScoresWindow";
 
@@ -18,10 +21,11 @@ const gameTypeOptions = {
 };
 
 storiesOf(HighScoresWindow.name, module)
+  .addDecorator(withReadme(Readme))
   .add("default", () => (
     <HighScoresWindow
       scores={scores}
-      gameType={select("Game Type", gameTypeOptions, GameType.Campaign)}
+      gameType={select("Game Type", gameTypeOptions, GameType.Standard)}
       onGameTypeChange={action("Game Type Change")}
       onExitClick={action("Exit Click")}
     />
--- packages/heroes-homm1-react/src/HighScoresWindow/README.md
+++ packages/heroes-homm1-react/src/HighScoresWindow/README.md
@@ -1,1 +1,23 @@
 # High Scores Window
+
+Displays standard and campaign game high scores.
+
+* displays standard game high scores by default
+
+## Standard
+
+![Standard](./assets/standard/enabled.png "Standard")
+
+* clicking it displays campaign game high scores.
+
+## Campaign
+
+![Campaign](./assets/campaign/enabled.png "Campaign")
+
+* clicking it display standard game high scores.
+
+## Exit
+
+![Exit](./assets/exit/enabled.png "Exit")
+
+Closes the window.
