bf1ca58d2986ad59b45cd4e7163e3d8febaef314
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2019-01-07 14:23:45 +0100

    add combat bar readme

--- packages/heroes-homm1-react/src/CombatWindow/CombatBar/CombatBar.story.tsx
+++ packages/heroes-homm1-react/src/CombatWindow/CombatBar/CombatBar.story.tsx
@@ -2,10 +2,14 @@ import { action } from "@storybook/addon-actions";
 import { text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { withReadme } from "storybook-readme";
+
+import Readme = require("./README.md");
 
 import { CombatBar } from "./CombatBar";
 
-storiesOf(`CombatWindow/${CombatBar.name}`, module)
+storiesOf("CombatWindow/CombatBar", module)
+  .addDecorator(withReadme(Readme))
   .add("default", () => (
     <CombatBar
       statusText={text("Status Text", "Content")}
---
+++ packages/heroes-homm1-react/src/CombatWindow/CombatBar/README.md
@@ -0,0 +1,13 @@
+# Combat Bar
+
+## Auto
+
+![Auto](./assets/auto/enabled.png "Auto")
+
+Allows the computer to control your troops for you.
+
+## Skip
+
+![Skip](./assets/skip/enabled.png "Skip")
+
+Passes your current troop's action.
