1e306a345b0cdd5ed32f188c7c83e9e87f63ae1f
tkryskiewicz <contact@tkryskiewicz.com>
2019-02-26 11:02:17 +0100

    replace custom typings for storybook-readme with DefinitelyTyped

--- packages/heroes-homm1-react/types/storybook-readme.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "storybook-readme" {
-  import { RenderFunction, StoryDecorator } from "@storybook/react";
-
-  function withReadme(readme: string | string[]): StoryDecorator;
-  function withReadme(readme: string | string[], callback: RenderFunction): RenderFunction;
-}
