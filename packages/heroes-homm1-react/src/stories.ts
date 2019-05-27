ed9ffb9073feea5aa5aa8f5fa9eb3ac93d8f47ee
tkryskiewicz <contact@tkryskiewicz.com>
2019-05-27 15:17:55 +0200

    simplify value selection in stories

--- packages/heroes-homm1-react/src/stories.ts
+++ packages/heroes-homm1-react/src/stories.ts
@@ -7,7 +7,6 @@ import {
   ArtifactId,
   CastleOptionStatus,
   CreatureId,
-  creatures,
   EditorObjectType,
   EditorOption,
   FarmStructureId,
@@ -16,7 +15,6 @@ import {
   GameOption,
   GameType,
   HeroClass,
-  heroes,
   HeroId,
   LuckType,
   MapObjectId,
@@ -38,122 +36,49 @@ import {
   TownId,
 } from "heroes-homm1";
 
-export interface SelectOptions {
-  readonly [s: string]: string;
-}
+export const gameOption = (name: string) =>
+  select<GameOption>(name, Object.values(GameOption), GameOption.NewGame);
 
-const gameOptionOptions = Object.keys(GameOption).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: GameDifficulty[c],
-  };
-}, {});
+export const gameType = (name: string) =>
+  select<GameType>(name, Object.values(GameType), GameType.Standard);
 
-export const gameOption = (name: string) => select(name, gameOptionOptions, GameOption.NewGame);
+export const gameDifficulty = (name: string) =>
+  select<GameDifficulty>(name, Object.values(GameDifficulty), GameDifficulty.Easy);
 
-const gameTypeOptions = {
-  Campaign: GameType.Campaign,
-  Standard: GameType.Standard,
-};
+export const alignment = (name: string) =>
+  select<Alignment>(name, Object.values(Alignment), Alignment.Red);
 
-export const gameType = (name: string) => select(name, gameTypeOptions, GameType.Standard);
+export const resource = (name: string) =>
+  select<Resource>(name, Object.values(Resource), Resource.Gold);
 
-const gameDifficultyOptions = Object.keys(GameDifficulty).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: GameDifficulty[c],
-  };
-}, {});
+export const mine = (name: string) =>
+  select<MapObjectId>(name, Object.values(resources).map((r) => r.mine) as MapObjectId[], MapObjectId.OreMine);
 
-export const gameDifficulty = (name: string) => select(name, gameDifficultyOptions, GameDifficulty.Easy);
+export const heroClass = (name: string) =>
+  select<HeroClass>(name, Object.values(HeroClass), HeroClass.Knight);
 
-const alignmentOptions = Object.keys(Alignment).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: Alignment[c],
-  };
-}, {});
-
-export const alignment = (name: string) => select(name, alignmentOptions, Alignment.Red);
-
-const resourceOptions = Object.keys(Resource).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: Resource[c],
-  };
-}, {});
-
-export const resource = (name: string) => select(name, resourceOptions, Resource.Gold);
-
-// TODO: keys are also values, doesn't look nice
-const mineOptions = Object.values(resources).reduce((p, c) => ({
-  ...p,
-  [c.mine]: c.mine,
-}), {});
-
-export const mine = (name: string) => select(name, mineOptions, MapObjectId.OreMine);
-
-const heroClassOptions = Object.keys(HeroClass).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: HeroClass[c],
-  };
-}, {});
-
-export const heroClass = (name: string) => select(name, heroClassOptions, HeroClass.Knight);
-
-const skillOptions = Object.keys(Skill).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: Skill[c],
-  };
-}, {});
-
-export const skill = (name: string) => select(name, skillOptions, Skill.Attack);
+export const skill = (name: string) =>
+  select<Skill>(name, Object.values(Skill), Skill.Attack);
 
 export const skillValue = (name: string) => number(name, 0, { range: true, min: 0, max: 999, step: 1 });
 
 export const morale = (name: string) => number(name, 0, { range: true, min: -3, max: 3, step: 1 });
 
-const moraleTypeOptions = Object.keys(MoraleType).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: MoraleType[c],
-  };
-}, {});
-
-export const moraleType = (name: string) => select(name, moraleTypeOptions, MoraleType.Good);
+export const moraleType = (name: string) =>
+  select<MoraleType>(name, Object.values(MoraleType), MoraleType.Good);
 
 export const luck = (name: string) => number(name, 0, { range: true, min: -3, max: 3, step: 1 });
 
-const luckTypeOptions = Object.keys(LuckType).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: LuckType[c],
-  };
-}, {});
-
-export const luckType = (name: string) => select(name, luckTypeOptions, LuckType.Good);
+export const luckType = (name: string) =>
+  select<LuckType>(name, Object.values(LuckType), LuckType.Good);
 
 export const troopIndex = (name: string) => number(name, 0, { range: true, min: 0, max: ArmySize - 1, step: 1 });
 
-const heroOptions = heroes.reduce<SelectOptions>((p, c) => {
-  return {
-    ...p,
-    [c.id]: c.id,
-  };
-}, {});
+export const hero = (name: string) =>
+  select<HeroId>(name, Object.values(HeroId), HeroId.LordKilburn);
 
-export const hero = (name: string) => select(name, heroOptions, HeroId.LordKilburn);
-
-const townOptions = Object.keys(TownId).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: TownId[c],
-  };
-}, {});
-
-export const town = (name: string) => select(name, townOptions, TownId.Farm);
+export const town = (name: string) =>
+  select<TownId>(name, Object.values(TownId), TownId.Farm);
 
 const structureOptions = {
   ...StructureId,
@@ -163,159 +88,70 @@ const structureOptions = {
   ...MountainsStructureId,
 };
 
-export const structure = (name: string) => select(name, structureOptions, StructureId.Castle);
+export const structure = (name: string) =>
+  select(name, structureOptions, StructureId.Castle);
 
-const castleOptionStatusOptions = Object.keys(CastleOptionStatus).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: CastleOptionStatus[c],
-  };
-}, {});
+export const castleOptionStatus = (name: string) =>
+  select<CastleOptionStatus>(name, Object.values(CastleOptionStatus), CastleOptionStatus.Built);
 
-export const castleOptionStatus = (name: string) => select(name, castleOptionStatusOptions, CastleOptionStatus.Built);
+export const creature = (name: string) =>
+  select<CreatureId>(name, Object.values(CreatureId), CreatureId.Peasant);
 
-const creatureOptions = creatures.reduce<SelectOptions>((p, c) => {
-  return {
-    ...p,
-    [c.id]: c.id,
-  };
-}, {});
+export const soundVolume = (name: string) =>
+  number(name, SoundVolume.On, {
+    max: SoundVolume.On,
+    min: SoundVolume.Off,
+    range: true,
+    step: 1,
+  });
 
-export const creature = (name: string) => select(name, creatureOptions, CreatureId.Peasant);
+export const movementSpeed = (name: string) =>
+  select<MovementSpeed>(name, Object.values(MovementSpeed), MovementSpeed.Walk);
 
-export const movementSpeedOptions = Object.keys(MovementSpeed).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: MovementSpeed[c],
-  };
-}, {});
+export const opponentSetting = (name: string) =>
+  select<OpponentSetting>(name, Object.values(OpponentSetting), OpponentSetting.Dumb);
 
-export const soundVolume = (name: string) => number(name, SoundVolume.On, {
-  max: SoundVolume.On,
-  min: SoundVolume.Off,
-  range: true,
-  step: 1,
-});
+export const artifact = (name: string) =>
+  select<ArtifactId>(name, Object.values(ArtifactId), ArtifactId.ThunderMaceOfDominion);
 
-export const movementSpeed = (name: string) => select(name, movementSpeedOptions, MovementSpeed.Walk);
+export const terrainType = (name: string) =>
+  select<TerrainType>(name, Object.values(TerrainType), TerrainType.Water);
 
-const opponentSettingOptions = Object.keys(OpponentSetting).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: OpponentSetting[c],
-  };
-}, {});
+export const combatSide = (name: string) =>
+  select<CombatSide>(name, Object.values(CombatSide), CombatSide.Attacker);
 
-export const opponentSetting = (name: string) => select(name, opponentSettingOptions, OpponentSetting.Dumb);
+export const scenarioSize = (name: string) =>
+  select<ScenarioSize>(name, Object.values(ScenarioSize), ScenarioSize.Small);
 
-const artifactOptions = Object.keys(ArtifactId).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: ArtifactId[c],
-  };
-}, {});
+export const scenarioDifficulty = (name: string) =>
+  select<ScenarioDifficulty>(name, Object.values(ScenarioDifficulty), ScenarioDifficulty.Easy);
 
-export const artifact = (name: string) => select(name, artifactOptions, ArtifactId.ThunderMaceOfDominion);
+export const spellType = (name: string) =>
+  select<SpellType>(name, Object.values(SpellType), SpellType.Combat);
 
-const terrainTypeOptions = Object.keys(TerrainType).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: TerrainType[c],
-  };
-}, {});
+export const spell = (name: string) =>
+  select<SpellId>(name, Object.values(SpellId), SpellId.Bless);
 
-export const terrainType = (name: string) => select(name, terrainTypeOptions, TerrainType.Water);
+export const mapObject = (name: string) =>
+  select<MapObjectId>(name, Object.values(MapObjectId), MapObjectId.Bouy);
 
-const combatSideOptions = Object.keys(CombatSide).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: CombatSide[c],
-  };
-}, {});
-
-export const combatSide = (name: string) => select(name, combatSideOptions, CombatSide.Attacker);
-
-const scenarioSizeOptions = Object.keys(ScenarioSize).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: ScenarioSize[c],
-  };
-}, {});
-
-export const scenarioSize = (name: string) => select(name, scenarioSizeOptions, ScenarioSize.Small);
-
-export const scenarioDifficultyOptions = Object.keys(ScenarioDifficulty).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: ScenarioDifficulty[c],
-  };
-}, {});
-
-export const scenarioDifficulty = (name: string) => select(name, scenarioDifficultyOptions, ScenarioDifficulty.Easy);
-
-const spellTypeOptions = Object.keys(SpellType).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: SpellType[c],
-  };
-}, {});
-
-export const spellType = (name: string) => select(name, spellTypeOptions, SpellType.Combat);
-
-const spellOptions = Object.keys(SpellId).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: SpellId[c],
-  };
-}, {});
-
-export const spell = (name: string) => select(name, spellOptions, SpellId.Bless);
-
-const mapObjectOptions = Object.keys(MapObjectId).reduce<SelectOptions>((p, c: any) => ({
-  ...p,
-  [c]: MapObjectId[c],
-}), {});
-
-export const mapObject = (name: string) => select(name, mapObjectOptions, MapObjectId.Bouy);
-
-const mapObjectSizeOptions = {
-  Large: "large",
-  Small: "small",
-};
-
-export const mapObjectSize = (name: string) => select(name, mapObjectSizeOptions, "large");
-
-const mapObjectOrientationOptions = Object.keys(MapObjectOrientation).reduce<SelectOptions>((p, c: any) => ({
-  ...p,
-  [c]: MapObjectOrientation[c],
-}), {});
+export const mapObjectSize = (name: string) =>
+  select(name, ["large", "small"], "large");
 
 export const mapObjectOrientation = (name: string) =>
-  select(name, mapObjectOrientationOptions, MapObjectOrientation.North);
+  select<MapObjectOrientation>(name, Object.values(MapObjectOrientation), MapObjectOrientation.North);
 
 const dwellingObjectOptions = [
   MapObjectId.Cottage,
   MapObjectId.Hut,
   MapObjectId.ThatchedHut,
-].reduce<SelectOptions>((p, c) => ({
-  ...p,
-  [c]: c,
-}), {});
+];
 
-export const dwellingObject = (name: string) => select(name, dwellingObjectOptions, MapObjectId.Cottage);
+export const dwellingObject = (name: string) =>
+  select<MapObjectId>(name, dwellingObjectOptions, MapObjectId.Cottage);
 
-const editorOptionOptions = Object.keys(EditorOption).reduce<SelectOptions>((p, c: any) => {
-  return {
-    ...p,
-    [c]: EditorOption[c],
-  };
-}, {});
+export const editorOption = (name: string) =>
+  select<EditorOption>(name, Object.values(EditorOption), EditorOption.Terrains);
 
-export const editorOption = (name: string) => select(name, editorOptionOptions, EditorOption.Terrains);
-
-const editorObjectTypeOptions = Object.keys(EditorObjectType).reduce<SelectOptions>((p, c: any) => ({
-  ...p,
-  [c]: EditorObjectType[c],
-}), {});
-
-export const editorObjectType = (name: string) => select(name, editorObjectTypeOptions, EditorObjectType.WaterObjects);
+export const editorObjectType = (name: string) =>
+  select<EditorObjectType>(name, Object.values(EditorObjectType), EditorObjectType.WaterObjects);
